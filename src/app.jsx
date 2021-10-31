a05356c44ef5ab32c6120e047a4f35c2765cf99c
SeYoung <wnl383@naver.com>
2021-10-31 16:39:34 +0900

    imported firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,62 @@
 import './app.css';
+import fire from './firebase.js'
+import { GoogleAuthProvider } from "firebase/auth";
+import React, {useState, useEffect } from 'react';
+import { getAuth, signInWithPopup } from "firebase/auth";
+
 
 function App() {
-  return 
+  const handleLogin = ()=>{
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+  const handlieSignup = ()=>{
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  }
+  const [user, setUser] = useState('');
+  return ( 
+    <React.Fragment>
+      <h1>Hello</h1>
+      <button onClick={handleLogin}>login</button>
+    </React.Fragment>
+  );
 }
 
 export default App;
